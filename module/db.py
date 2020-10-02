7c50ec40da29c17806509a04cd5acddb75741101
huangyezhou <2498144289@qq.com>
2020-10-02 19:13:59 +0800

    add db.py and teahcer.py

---
+++ module/db.py
@@ -0,0 +1,29 @@
+import pymysql
+
+
+
+
+class DB:
+    def __init__(self):
+        self.address = "localhost"
+        self.username = "root"
+        self.password = "root"
+        self.database = "test"
+
+
+    def connection(self):
+        self.db=pymysql.connect(self.address,self.username,self.password,self.database)
+        self.cursor=self.db.cursor()
+
+        return self.cursor
+
+    def close(self):
+        self.db.close()
+
+if __name__ == '__main__':
+    db=DB()
+    cursor=db.connection()
+    print(db.db)
+    db.close()
+    print(db.db)
+
